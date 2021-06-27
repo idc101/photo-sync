1eebad5d7d6257943dedf81e5450ca887ea440e6
Iain Cardnell <idc101@users.noreply.github.com>
2021-06-27 20:10:29 +1000

    extract xmp data

--- src/main/kotlin/net/cardnell/main.kt
+++ src/main/kotlin/net/cardnell/main.kt
@@ -1,13 +1,17 @@
 package net.cardnell
 
 import com.drew.imaging.ImageMetadataReader
+import com.drew.metadata.Metadata
+import com.drew.metadata.exif.ExifSubIFDDirectory
+import com.drew.metadata.xmp.XmpDirectory
 import java.io.File
 import java.nio.file.Paths
 import java.time.LocalDateTime
 
 
 fun main(): Unit {
-    val metadata = ImageMetadataReader.readMetadata(File("D:\\Pictures-Originals\\2012\\2012-06 Family BBQ\\DSC_0524.JPG"))
+    //val metadata = ImageMetadataReader.readMetadata(File("D:\\Pictures-Originals\\2012\\2012-06 Family BBQ\\DSC_0524.JPG"))
+    val metadata = ImageMetadataReader.readMetadata(File("D:\\Pictures-Originals\\2002\\2002-01 Spring Term 2nd Year\\Image07.jpg"))
     for (directory in metadata.directories) {
         for (tag in directory.tags) {
             //if (tag.description.contains("Mark Bridges")) {
@@ -15,6 +19,9 @@ fun main(): Unit {
             //}
         }
     }
+    val directory = metadata.getFirstDirectoryOfType<XmpDirectory>(XmpDirectory::class.java)
+    directory.xmpProperties.forEach { t, u -> println("$t = $u")}
+
 //    val yearDirs = Paths.get("D:\Pictures-Originals").toFile().listFiles().filter { it.isDirectory }
 //    yearDirs.forEach { yearDir ->
 //        val picFolders = yearDir.listFiles().filter { it.isDirectory }
@@ -53,10 +60,17 @@ fun isFriendOrFamily(picture: File): Boolean {
 
 fun getRating(): Int {
     //[Exif IFD0] Rating - 3
+    return 1
 }
 
-fun getDateTimeOriginal(): LocalDateTime {
+fun getDateTimeOriginal(metadata: Metadata): LocalDateTime {
     //[Exif SubIFD] Date/Time Original - 2012:06:16 14:59:34
+    // obtain the Exif directory
+    val directory = metadata.getFirstDirectoryOfType<ExifSubIFDDirectory>(ExifSubIFDDirectory::class.java)
+
+    // query the tag's value
+    val date = directory.getDate(ExifSubIFDDirectory.TAG_DATETIME_ORIGINAL)
+    return LocalDateTime.now()
 }
 
 val friendsOrFamily = setOf(
